be62e2e234f226298ab9e3c9dee4b1496b5d3a6d
mifta <smiftakhus@gmail.com>
2021-10-10 22:52:42 +0700

    <feat>(common): display currency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DisplayCurrency from './content/DisplayCurrency';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DisplayCurrency />
   </React.StrictMode>,
   document.getElementById('root')
 );
